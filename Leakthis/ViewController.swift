cbfcaeee61fc0fd32960dd4cefd8a46a56f13779
Charlton Smith <CharltonSmith@outlook.com>
2020-10-22 01:19:12 -0400

    Initial Commit

---
+++ Leakthis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Leakthis
+//
+//  Created by Charlton Smith on 10/22/20.
+//  Copyright © 2020 Charlton Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LeakthisTests/LeakthisTests.swift
@@ -0,0 +1,34 @@
+//
+//  LeakthisTests.swift
+//  LeakthisTests
+//
+//  Created by Charlton Smith on 10/22/20.
+//  Copyright © 2020 Charlton Smith. All rights reserved.
+//
+
+import XCTest
+@testable import Leakthis
+
+class LeakthisTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
